79b2efc458abcd153d59c9804b3f443c43f0292e
47chen <x841010@gmail.com>
2024-01-10 15:07:47 -0500

    switch init array--todoList=localS.get(key)||[]-- into listContainer.innerHTML to get list-items directly w/o redundant code

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -3,40 +3,53 @@ let listContainer = document.querySelector("[data-list-container]");
 let input = document.querySelector("[data-input]");
 const LOCAL_STORAGE_KEY = "todo.lists";
 
-let todoLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
-
-function render() {
-  listContainer.innerHTML = "";
-  // why we set this to empty string?
-  // because we want to clear the listContainer before we render the new todo
-  todoLists.forEach((todo) => {
-    let newTodo = document.createElement("li");
-    newTodo.classList.add("todo-item");
-    newTodo.innerText = todo;
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    newTodo.appendChild(span);
-    listContainer.appendChild(newTodo);
-  });
-}
-
-function clearInput() {
-  input.value = "";
-}
+let curList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 
 function addTodo() {
   if (input.value === "") {
     alert("You must write something!");
     return;
+  } else {
+    let li = document.createElement("li");
+    li.classList.add("todo-item");
+    li.innerText = input.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
-  todoLists.push(input.value);
   clearInput();
   save();
-  render();
+}
+
+// function render() {
+//   listContainer.innerHTML = "";
+//   // why we set this to empty string?
+//   // because we want to clear the listContainer before we render the new todo
+//   todoLists.forEach((todo) => {
+//     let newTodo = document.createElement("li");
+//     newTodo.classList.add("todo-item");
+//     newTodo.innerText = todo;
+//     let span = document.createElement("span");
+//     span.innerHTML = "\u00d7";
+//     newTodo.appendChild(span);
+//     listContainer.appendChild(newTodo);
+//   });
+// }
+
+function clearInput() {
+  input.value = "";
 }
 
 function save() {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoLists));
+  localStorage.setItem(
+    LOCAL_STORAGE_KEY,
+    JSON.stringify(listContainer.innerHTML)
+  );
+}
+
+function render() {
+  listContainer.innerHTML = curList;
 }
 
 // TODO : check todoList
@@ -44,10 +57,9 @@ function save() {
 listContainer.addEventListener(
   "click",
   (e) => {
-    console.log(todoLists);
     if (e.target.tagName.toLowerCase() === "li") {
       e.target.classList.toggle("checked");
-      // save();
+      save();
     } else if (e.target.tagName.toLowerCase() === "span") {
       e.target.parentElement.remove();
       save();
