29b847652eb37df5b9085fed0f14c651b21aac34
47chen <x841010@gmail.com>
2024-01-10 13:18:12 -0500

    save() function via storing todoLists in localStorage

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -1,11 +1,9 @@
 // TODO 1: get DOM elements for reference
-let input = document.querySelector("[data-input]");
 let listContainer = document.querySelector("[data-list-container]");
-let todoLists = [
-  "create addTodo func",
-  "create render func",
-  "create delete func",
-];
+let input = document.querySelector("[data-input]");
+const LOCAL_STORAGE_KEY = "todo.lists";
+
+let todoLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 
 // TODO 2: render the list of todos
 function render() {
@@ -26,8 +24,13 @@ function clearInput() {
 
 // TODO 3: add new todo function
 function addTodo() {
+  if (input.value === "") {
+    alert("You must write something!");
+    return;
+  }
   todoLists.push(input.value);
   clearInput();
+  save();
   render();
 }
 
@@ -35,3 +38,11 @@ render();
 // TODO 4: delete todo function -- use span to delete todo
 
 // TODO 5: Save() using localStorage
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoLists));
+  // console.log(todoLists);
+  // console.log(JSON.stringify(todoLists));
+  // console.log(localStorage.getItem(LOCAL_STORAGE_KEY));
+  // console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+}
