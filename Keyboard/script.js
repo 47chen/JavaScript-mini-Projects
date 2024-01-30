a1c6a865d792d92c0b5cadbae6b2b83ea1731add
47chen <x841010@gmail.com>
2024-01-30 16:53:31 -0500

    key-events + remove-child

--- Keyboard/script.js
+++ Keyboard/script.js
@@ -10,47 +10,11 @@ steps
 */
 
 const $box = document.getElementById("box");
-
-// document.onkeydown = function (event) {
-//   // init divMove for the speed of moving box
-//   let divMove = 10;
-//   // if press CTRL, speed up
-//   if (event.ctrlKey) {
-//     console.log("Press CTRL, sit tied, bro!");
-//     divMove = 50;
-//   } else if (event.altKey) {
-//     console.log("Press ALT, Lets go home bro!");
-//     $box.style.left = 0;
-//     $box.style.top = 0;
-//   } else if (event.shiftKey) {
-//     let isGreen = $box.style.backgroundColor === "green";
-//     console.log("Press Shift, 變色龍來了!");
-//     if (!isGreen) $box.style.backgroundColor = "green";
-//     if (isGreen) $box.style.backgroundColor = "tomato";
-//   }
-//   console.log("keyCode" + event.keyCode);
-//   switch (event.keyCode) {
-//     // left
-//     case 37:
-//       $box.style.left = $box.offsetLeft - divMove + "px";
-//       break;
-//     // top
-//     case 38:
-//       $box.style.top = $box.offsetTop - divMove + "px";
-//       break;
-//     // right
-//     case 39: {
-//       $box.style.left = $box.offsetLeft + divMove + "px";
-//       break;
-//     }
-//     // down
-//     case 40: {
-//       $box.style.top = $box.offsetTop + divMove + "px";
-//       break;
-//     }
-//   }
-// };
-
+/* --- addEventListener with keydown */
+/* --- we can also write 
+document.onkeydown = function(event) {...}
+<button onclick="handleClick()"> === addEventListener('click', handleClick());
+*/
 // document.addEventListener("keydown", (event) => {
 //   // init divMove for the speed of moving box
 //   let divMove = 10;
@@ -104,8 +68,10 @@ const domKeyBoard = (event) => {
   } else if (event.shiftKey) {
     let isGreen = $box.style.backgroundColor === "green";
     console.log("Press Shift, 變色龍來了!");
-    if (!isGreen) $box.style.backgroundColor = "green";
-    if (isGreen) $box.style.backgroundColor = "tomato";
+    $box.style.backgroundColor = "green";
+    isGreen
+      ? ($box.style.backgroundColor = "tomato")
+      : ($box.style.backgroundColor = "green");
   }
   console.log("Press " + event.key + " Key");
   switch (event.key) {
