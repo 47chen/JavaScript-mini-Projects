cbd2ee6489b8246063cdc8b95cc27b0d961c80bc
47chen <x841010@gmail.com>
2024-02-01 19:28:30 -0500

    Implement promise.all - ⭐️return array of promise

--- JavaScript-Practice/re-scripts.js
+++ JavaScript-Practice/re-scripts.js
@@ -1,60 +1,60 @@
 /* 1. 🔥 Mock fetching data using promise - .then().catch() | async await try catch 🔥 */
-const fetchData = (url) => {
-  return new Promise((resolve, reject) => {
-    if (url === "47chen.com") {
-      setTimeout(() => {
-        resolve("Successfully fetch the data!");
-      }, 1000);
-    } else reject("Something went wrong");
-  });
-};
-const url1 = "47chen.com";
-const url2 = "48chen.com";
-// .then .catch
-fetchData(url1)
-  .then((message) => console.log(message))
-  .catch((err) => console.log(err));
+// const fetchData = (url) => {
+//   return new Promise((resolve, reject) => {
+//     if (url === "47chen.com") {
+//       setTimeout(() => {
+//         resolve("Successfully fetch the data!");
+//       }, 1000);
+//     } else reject("Something went wrong");
+//   });
+// };
+// const url1 = "47chen.com";
+// const url2 = "48chen.com";
+// // .then .catch
+// fetchData(url1)
+//   .then((message) => console.log(message))
+//   .catch((err) => console.log(err));
 
-fetchData(url2)
-  .then((message) => console.log(message))
-  .catch((err) => console.log(err));
+// fetchData(url2)
+//   .then((message) => console.log(message))
+//   .catch((err) => console.log(err));
 
-// async await try catch
-const getRequest = async (url) => {
-  try {
-    const message = await fetchData(url);
-    console.log(message);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// // async await try catch
+// const getRequest = async (url) => {
+//   try {
+//     const message = await fetchData(url);
+//     console.log(message);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-getRequest(url1);
-getRequest(url2);
+// getRequest(url1);
+// getRequest(url2);
 
-// Implement a new promise
-const myPromise = new Promise((resolve, reject) => {
-  const someCondition = true;
-  if (someCondition) {
-    setTimeout(() => {
-      resolve("resolve my promise");
-    }, 1000);
-  } else reject("Error fetching data");
-});
+// // Implement a new promise
+// const myPromise = new Promise((resolve, reject) => {
+//   const someCondition = true;
+//   if (someCondition) {
+//     setTimeout(() => {
+//       resolve("resolve my promise");
+//     }, 1000);
+//   } else reject("Error fetching data");
+// });
 
-async function resolveMyPromise() {
-  try {
-    const data = await myPromise;
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
-resolveMyPromise();
-setTimeout(
-  () => console.log("* ------------------------------------- *"),
-  2000
-);
+// async function resolveMyPromise() {
+//   try {
+//     const data = await myPromise;
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// resolveMyPromise();
+// setTimeout(
+//   () => console.log("* ------------------------------------- *"),
+//   2000
+// );
 /* ------------------------------------- */
 /* 2. 🔥 shallowCopy and deepyCopy 🔥
 
@@ -69,26 +69,26 @@ ex. B object(animal: 'cat, age:'2'); => B reference
 - ⭕️ To Create Deep Copy - const deep_copy = JSON.parse(JSON.stringify(original_obj))
 
 */
-function shallowCopy(originObj) {
-  return Object.assign({}, originObj);
-}
-const original_obj = { a: 1, b: { c: 2, d: { e: 5 } } };
-const shallow_copy = shallowCopy(original_obj);
+// function shallowCopy(originObj) {
+//   return Object.assign({}, originObj);
+// }
+// const original_obj = { a: 1, b: { c: 2, d: { e: 5 } } };
+// const shallow_copy = shallowCopy(original_obj);
 
-const unknownCopy = { ...shallow_copy };
-console.log("* ------------------------------------- *");
-console.log(shallow_copy === original_obj); // false
-console.log(shallow_copy == original_obj); // false
-console.log("* Compare top level props | nested obj in object | deep value *");
-console.log(shallow_copy.a === original_obj.a); // true
-console.log(shallow_copy.b === original_obj.b); // false ❌ // TRUE ✅
-console.log(shallow_copy.b.d.e === original_obj.b.d.e); // false❌ // TRUE ✅
-console.log("* ------Reference changed check ------ *");
-shallow_copy.a = 100;
-original_obj.a = 500;
-console.log(shallow_copy.a, original_obj.a); // 100, 500 since Top Level Props pass by value
-shallow_copy.b.c = 100000;
-console.log(shallow_copy, original_obj);
+// const unknownCopy = { ...shallow_copy };
+// console.log("* ------------------------------------- *");
+// console.log(shallow_copy === original_obj); // false
+// console.log(shallow_copy == original_obj); // false
+// console.log("* Compare top level props | nested obj in object | deep value *");
+// console.log(shallow_copy.a === original_obj.a); // true
+// console.log(shallow_copy.b === original_obj.b); // false ❌ // TRUE ✅ - top level props pass by value
+// console.log(shallow_copy.b.d.e === original_obj.b.d.e); // false❌ // TRUE ✅ - top level props pass by value
+// console.log("* ------Reference changed check ------ *");
+// shallow_copy.a = 100;
+// original_obj.a = 500;
+// console.log(shallow_copy.a, original_obj.a); // 100, 500 since Top Level Props pass by value
+// shallow_copy.b.c = 100000;
+// console.log(shallow_copy, original_obj);
 // { a: 100, b: { c: 100000, d: { e: 5 } } } { a: 500, b: { c: 100000, d: { e: 5 } } }
 /* 
 👉 1. '===' strict equals, even two object share the same reference,
@@ -96,12 +96,65 @@ they are still distinct object, return false
 👉 2. Only Top Level Property (第一層no matter value or object)都是pass by value
 only nested object inside pass by reference - ex. obj.a obj.b pass by value, obj.b.c - pass by reference
 */
-console.log("* ------------- Deep Copy ------------- *");
-function deepCopyFactory(obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
-const deepcopy_obj = deepCopyFactory(original_obj);
-original_obj.a = 1000;
-deepcopy_obj.a = 50000;
-console.log(original_obj.a, deepcopy_obj.a); // 1000, 50000 => different reference
-console.log("* ------------------------------------- *");
+// console.log("* ------------- Deep Copy ------------- *");
+// function deepCopyFactory(obj) {
+//   return JSON.parse(JSON.stringify(obj));
+// }
+// const deepcopy_obj = deepCopyFactory(original_obj);
+// original_obj.a = 1000;
+// deepcopy_obj.a = 50000;
+// console.log(original_obj.a, deepcopy_obj.a); // 1000, 50000 => different reference
+// console.log("* ------------------------------------- *");
+/* 3. 🔥 Implement Promise.all 🔥 */
+/* 🟢 Basic using Promise.all - iterable - * need array of promise 
+      promise -> resolve or reject -- use try/catch or .then().catch() to get the promise state
+*/
+console.log("* ---- Implement Promise.all() with build-in function -----");
+const promise1 = Promise.resolve("1");
+const promise2 = new Promise((res, rej) => res("2"));
+const promise3 = new Promise((resolve, rej) => {
+  setTimeout(() => {
+    resolve("3");
+  }, 2000);
+});
+const promise4 = new Promise((res, rej) =>
+  rej("Error, once there is a reject, promise.all would fail")
+);
+const promises = [promise1, promise2, promise3];
+
+const basicResult = Promise.all(promises);
+basicResult
+  .then((res) => console.log(res, "Promise.all()"))
+  .catch((err) => console.log(err));
+
+console.log("* -- Implement promise.all from scratch -- *");
+const promiseAll = function (promises_array) {
+  // edge case
+  if (!Array.isArray(promises_array)) return new Error("args must be iterable");
+  if (promises_array.length === 0) return Promise.resolve([]);
+
+  const results = [];
+  let resCounter = 0;
+
+  // ⭐️我沒想到的部分⭐️
+  return new Promise((resolve, reject) => {
+    promises_array.forEach((promise, index) => {
+      promise
+        .then((value) => {
+          results[index] = value; // or push value to results
+          resCounter += 1;
+          if (resCounter === promises_array.length) {
+            resolve(results);
+          }
+        })
+        .catch((err) => reject(new Error("Something went wrong!")));
+    });
+  });
+};
+
+const result = promiseAll([...promises]);
+result.then((res) => console.log(res)).catch((err) => console.log(err));
+const result2 = promiseAll([...promises, promise4]);
+result2.then((res) => console.log(res)).catch((err) => console.log(err));
+const result3 = promiseAll([promise2, promise3]);
+result3.then((res) => console.log(res)).catch((err) => console.log(err));
