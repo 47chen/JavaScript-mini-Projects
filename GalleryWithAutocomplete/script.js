97e16e39befca08ae9f0219ba87ce4429554bb50
47chen <x841010@gmail.com>
2024-02-02 16:14:02 -0500

    😎1st try -- fetch movies + display movies + currentTarget Event Delegation complete

--- GalleryWithAutocomplete/script.js
+++ GalleryWithAutocomplete/script.js
@@ -23,12 +23,87 @@ const fetchMovies = async () => {
     if (data.hasOwnProperty("movies")) {
       movieList = data.movies;
       console.log("Successfully fetch movie data", movieList);
+      // ✅ JS single thread, you should addMovies here, if you
+      //   addMovies outside of here, you will instantly envoke with
+      //   or create another async function 看下面
+      /* addMovies(movieList); */
     } else {
       console.log("No such property");
     }
+
+    return data.movies;
   } catch (error) {
     console.error("Error fetching movies", error);
   }
 };
 
-fetchMovies();
+/* 
+TODO:
+1. get DOM by reference
+2. movieList - appendChild to movies-wrapper
+3. assign api value to innerHTML | textContent
+*/
+
+const wrapper = document.getElementById("movies-wrapper");
+const addMovies = async (movies) => {
+  movies.forEach((movie) => {
+    let id = movie.id;
+    let link = movie.link;
+    let text = movie.text;
+    let imgUrl = movie.img;
+
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let movieId = document.createElement("h3");
+    let movieLink = document.createElement("p");
+    let movieText = document.createElement("p");
+
+    div.classList.add("movie-card");
+    img.classList.add("movie-img");
+    movieId.classList.add("movie-id");
+    movieText.classList.add("movie-text");
+    movieLink.classList.add("movie-link");
+
+    // ⭐️ assign value to innerHTML | textContent | innerText
+    movieId.innerText = `id: ${id}`;
+    movieLink.innerHTML = `<strong>Link</strong>: ${link}`;
+    movieText.textContent = `Text: ${text}`;
+
+    img.src = imgUrl;
+    img.alt = imgUrl;
+
+    div.appendChild(img);
+    div.appendChild(movieId);
+    div.appendChild(movieLink);
+    div.appendChild(movieText);
+
+    wrapper.appendChild(div);
+  });
+};
+
+const display = async () => {
+  let moviess = await fetchMovies();
+  addMovies(moviess);
+};
+
+display();
+
+// ⭐️⭐️⭐️
+// acutally we can add eventListener to each div in the addMovies functino
+
+// but using currentTarget Event Delegation can improve more efficiency
+wrapper.addEventListener("click", (e) => {
+  e.preventDefault();
+  const clickElement = e.target.closest(".movie-card");
+  console.log(clickElement);
+  if (clickElement) {
+    const movieId = clickElement.querySelector(".movie-id");
+    const text = movieId.innerText;
+    console.log(text);
+    const real_id = text.trim().split("id: ")[1];
+    console.log(real_id);
+    console.log(movieId);
+    // console.log(real_id);
+    alert(`Click on ${real_id}`);
+  }
+});
